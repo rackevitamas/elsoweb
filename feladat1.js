d9ca2cfba230706a75c8516829659943fa961508
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-09-12 14:25:04 +0200

    Add files via upload

---
+++ feladat1.js
@@ -0,0 +1,8 @@
+let tomb = [];
+function enter(){
+    let vnev = document.getElementById('vnev').value;
+    let knev = document.getElementById('knev').value;
+    tomb = [vnev, knev]
+    console.log(vnev + " " + knev);
+    console.log(tomb);
+}
