8f5f6eab17ff62351bb668410b693862ce0d590d
rackevitamas <90187820+rackevitamas@users.noreply.github.com>
2024-09-20 14:27:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TombokProjekt;
+
+Console.WriteLine("Hello, World!");
+
+int[] tomb = new int[] { 0, 1, 1, 0, 0, 0, 1 };
+int[] tomb2 = new int[] { 1, 2, 4, 5, 10, 3, 6 };
+int[] tomb3 = new int[] { 9, 3, 16, 4, 2, 6, 36 };
+Console.WriteLine(TombMuveletek.GetEgyesekSzama(tomb));
+Console.WriteLine(TombMuveletek.GetDbElozoKetSzamKetszerese(tomb2));
+Console.WriteLine(TombMuveletek.GetDbElozoKetSzamNegyzete(tomb3));
---
+++ TombMuveletek.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TombokProjekt
+{
+    public static class TombMuveletek
+    {
+        public static int GetEgyesekSzama(int[] tomb) 
+        {
+            int db = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] == 1)
+                {
+                    db++;
+                }
+            }
+            return db;
+        }
+        public static int GetDbElozoKetSzamKetszerese(int[] tomb) 
+        {
+            int db = 0;
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                if (tomb[i] == tomb[i - 1] * 2)
+                    db++;
+            }
+            return db; 
+        }
+        public static int GetDbElozoKetSzamNegyzete(int[] tomb)
+        {
+            int db = 0;
+            for (int i = 1; i < tomb.Length ; i++)
+            {
+                if (tomb[i] == Math.Pow(tomb[i - 1], 2))
+                    db++;
+            }
+            return db;
+        }
+        public static int GetElozoKora(int a, int b, int[] tomb)
+        {
+            string ak = "Az a-nak korábban szerepel!";
+            string bk = "A b-nek korábban szerepel!";
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] == a)
+                {
+                    if (tomb[i + 1] == b)
+                    {
+                        return ak;
+                    }
+                }
+                else if (tomb[i] == b)
+                {
+                    if (tomb[i + 1] == a)
+                    {
+                        return bk;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
